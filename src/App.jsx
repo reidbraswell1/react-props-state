661f2544c7e8d686a3466d748fdde73df9169be8
Reid Braswell <reidbras@gmail.com>
2022-09-01 16:35:48 -0500

    Adding render method to App.jsx, added constructor, added 2 properties to the state object, added unordered list to render method called component List to list the mapped items in the initial list array.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
-import './App.css';
+import "./css/App.css";
 import { Component } from 'react';
+import List from "./components/List.jsx";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    let list = ["ready", "set", "GO"];
+    let text = "";
+
+    this.state = {
+      list: list,
+      text: text,
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <ul>
+          {this.state.list.map((value,index, array) => {
+            console.log(value);
+              let listId = `List:${index}`;
+              let listText = value;
+              return <List key={index} id={listId} text={listText}></List>
+          })}
+        </ul>
       </div>
     );
   }
