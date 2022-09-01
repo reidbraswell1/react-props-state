e544a2ca97cf9f5500b265df7a5d3b8ae36d29d3
reid <reidbras@gmail.com>
2022-09-01 12:47:51 -0500

    Adding App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+export default App;
