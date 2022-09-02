c320b124427c464476f28e76a9b98772153716fe
Reid Braswell <reidbras@gmail.com>
2022-09-02 17:12:58 -0500

    Adding Delete button and correcting logic not to allow empty strings to be added to the list. Also added additional styling.

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,64 @@ class App extends Component {
     const list = ["ready", "set", "GO"];
     const text = "";
 
+    // Set initial state
     this.state = {
       list: list,
       text: text,
     }
 
+    // Bind functions
     this.handleOnChangeInput = this.handleOnChangeInput.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  // Input onChange event handler
   handleOnChangeInput(event) {
     console.log(`---Begin handleOnChangeInput()---`);
     this.setState({text:event.target.value});
     console.log(`---End handleOnChangeInput()---`)
   }
 
+  // Submit button event handler
   onSubmit(event) {
     console.log(`---Begin onSubmit()---`);
+    let buttonValue = event.target.value;
     event.preventDefault();
-    console.log(`This.text=${this.state.text}`);
-    let newList = this.state.list.slice(0);
-    newList.push(this.state.text)
-    this.setState({list:newList})
-    this.setState({text:""});
-    console.log(`This.state.list=${this.state.list}`);
+    switch (buttonValue) {
+      case "Add":
+        // Don't process empty string
+        if (!this.state.text || this.state.text.length == 0) {
+          console.log(`Empty String`);
+        }
+        else {
+          // Must use a new array
+          let newList = this.state.list.slice(0);
+          newList.push(this.state.text)
+          this.setState({list:newList})
+          this.setState({text:""});
+        }
+        break;
+      case "Delete":
+        if(!this.state.text || this.state.text.length == 0) {
+          console.log(`Empty String`);
+        }
+        else {
+          let newList = [];
+          let deleteIndex = parseInt(this.state.text);
+          for(let i=0; i < this.state.list.length; i++) {
+            if(deleteIndex === i + 1) {
+
+            }
+            else {
+              newList.push(this.state.list[i]);
+            }
+          }
+          this.setState({list:newList});
+          this.setState({text:""});
+        }
+        break; 
+
+    }
     console.log(`---End onSubmit()---`);
   }
 
@@ -46,11 +80,13 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="col-3" style={{ marginLeft: 'auto', marginRight: 'auto'}}>
-            <form onSubmit={this.onSubmit}>
+            <form onClick={this.onSubmit}>
+            <p>To delete an item enter it's number only!</p>
               <div className="form-group">
                 <label htmlFor="toDoInput">Add to do item </label>
-                <input id="toDoInput" className="form-control" value={this.state.text} onChange={this.handleOnChangeInput}></input>
-                <button className="mt-2" type="submit">Add</button>
+                <input id="toDoInput" className="mb-1 form-control" value={this.state.text} onChange={this.handleOnChangeInput}></input>
+                <button className="m-2 btn btn-primary" type="submit" name="Add" value="Add">Add</button>
+                <button className="m-2 btn btn-danger" type="submit" name="Delete" value="Delete">Delete</button>
               </div>
             </form>
           </div>
@@ -64,9 +100,8 @@ class App extends Component {
           <div className="col-3 border border-primary rounded mt-4" style={{ marginLeft: 'auto', marginRight: 'auto'}}>
             <ul className="list-group mt-2 mb-2">
               {this.state.list.map((value,index, array) => {
-                console.log(value);
                 let listId = `List:${index}`;
-                let listText = value;
+                let listText = `(${index+1}.) ${value}`;
                 return <List key={index} id={listId} text={listText}></List>
               })}
             </ul>
