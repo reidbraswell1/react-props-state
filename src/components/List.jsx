3a4192cde3ebe6740d56bf9f124fed74b1fa3e89
reid <reidbras@gmail.com>
2022-09-02 20:14:15 -0500

    Adding bootstrap style

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
 const List = (props) => {
-    return (<li id={props.id}>{props.text}</li>);
+    return (<li className="list-group-item" id={props.id}>{props.text}</li>);
 }
 export default List;
