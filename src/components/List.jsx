9ba5577f761e019c5a4f26f089d67337a814ec9d
reid <reidbras@gmail.com>
2022-09-04 21:21:42 -0500

    Adding class component method.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,20 @@
+import { render } from "@testing-library/react";
 import { Component } from "react";
 
+// Functional Component Method
+/*
 const List = (props) => {
     return (<li className="list-group-item" id={props.id}>{props.text}</li>);
+}
+*/
+// Class Component Method
+class List extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (<li className="list-group-item" id="this.props.id">{this.props.text}</li>)
+    }   
 }
 export default List;
